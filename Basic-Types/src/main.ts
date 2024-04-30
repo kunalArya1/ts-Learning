6485730d8fdac02ebc37cc2f5902f9f0f4bb163a
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-04-30 12:45:27 +0530

    Array in ts

--- Basic-Types/src/main.ts
+++ Basic-Types/src/main.ts
@@ -5,3 +5,18 @@ let a: number = 4;
 let naam: string = "Kunal Kumar";
 
 // just simple like this we can use the basic types in typescript
+
+const num: string[] = ["Kunal", "Kumar", "Singh"];
+
+num.push("Kunal");
+
+
+// num.push(1);
+ // this will give error because we have defined the array as string
+
+ const c: [1,2,3,4] = [1,2,3,4];
+//  c.push(5);
+ // this will give error because we have defined the array as fixed length array
+
+const b: number[] = [1, 2, 3, 4, 5];
+const date: Date[] = [new Date(), new Date()];
