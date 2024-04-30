18e2d045067b57aeb22c44afc3b724de47745b3e
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-04-30 12:48:02 +0530

    Any Types in ts

--- Basic-Types/src/main.ts
+++ Basic-Types/src/main.ts
@@ -20,3 +20,16 @@ num.push("Kunal");
 
 const b: number[] = [1, 2, 3, 4, 5];
 const date: Date[] = [new Date(), new Date()];
+
+// Any type :- 
+let randomValue: any = 10;
+randomValue = true;
+randomValue = "Kunal";
+// unknown type :- 
+let randomValue2: unknown = 10;
+randomValue2 = true;
+randomValue2 = "Kunal"; 
+// unknown type is similar to any type but it is not allow to assign any value to it directly like any type
+
+// randomValue2 = 10; 
+// this will give error because we have defined the type as unknown and we can not assign any value to it directly  
